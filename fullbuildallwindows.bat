b282264c590b485ee19840c2fa071a35d4f3c314
Gavin Isgar <gisgar3@gmail.com>
2018-04-18 21:08:18 -0400

    Fix Windows Assembly Errors
    Fixed Windows assembly errors.

---
+++ fullbuildallwindows.bat
@@ -0,0 +1,11 @@
+cd desktop/opensource/gritz
+dotnet publish -r win7-x64 -c Release
+dotnet publish -r win7-x86 -c Release
+dotnet publish -r win8-x64 -c Release
+dotnet publish -r win8-x86 -c Release
+dotnet publish -r win81-x64 -c Release
+dotnet publish -r win81-x86 -c Release
+dotnet publish -r win10-x64 -c Release
+dotnet publish -r win10-x86 -c Release
+dotnet publish -r win10-arm -c Release
+dotnet publish -r win10-arm64 -c Release
