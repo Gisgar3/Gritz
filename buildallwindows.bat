0dad10de83b3252c60d1a3fd72323f5c485028bc
Gavin Isgar <gisgar3@gmail.com>
2018-04-18 20:52:14 -0400

    Add More Windows Executables
    Added more Windows executable files for testing.

---
+++ buildallwindows.bat
@@ -0,0 +1,11 @@
+cd desktop/opensource/gritz
+dotnet build -r win7-x64 -c Release
+dotnet build -r win7-x86 -c Release
+dotnet build -r win8-x64 -c Release
+dotnet build -r win8-x86 -c Release
+dotnet build -r win81-x64 -c Release
+dotnet build -r win81-x86 -c Release
+dotnet build -r win10-x64 -c Release
+dotnet build -r win10-x86 -c Release
+dotnet build -r win10-arm -c Release
+dotnet build -r win10-arm64 -c Release
